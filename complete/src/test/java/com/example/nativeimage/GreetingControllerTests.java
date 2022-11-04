f31ce448db618e69b9362da79d5bfb358deea6e7
Dave Syer <dsyer@vmware.com>
2022-11-04 10:46:27 +0000

    Update with tests and Gradle plugins

--- complete/src/test/java/com/example/nativeimage/GreetingControllerTests.java
+++ complete/src/test/java/com/example/nativeimage/GreetingControllerTests.java
@@ -21,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,7 +28,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisabledInNativeImage
 public class GreetingControllerTests {
 
 	@Autowired
--- complete/src/test/java/com/example/nativeimage/NativeImageApplicationTests.java
+++ complete/src/test/java/com/example/nativeimage/NativeImageApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class NativeImageApplicationTests {
+class NativeImageApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
